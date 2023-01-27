4f1eee522b3448cae2d0169fadd33aace8b86f99
Cedric Men <cedricm13@outlook.com>
2023-01-26 22:12:44 -0800

    added top bar color support for iphones

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,15 @@ import "../styles/globals.css";
 
 import Navigation from "../components/navigation/navigation";
 import FooterNote from "../components/footer/customFooter";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return(
     <div className="flex flex-col bg-gradient-to-l from-[#4B79A1] to-[#283E51] min-h-screen gap-3 items-center">
+      <Head>
+        <meta name='theme-color' content='#4B79A1' media="(prefers-color-scheme: light)"></meta>
+        <meta name='theme-color' content='#283E51' media="(prefers-color-scheme: dark)"></meta>
+      </Head>
       <Navigation></Navigation>
       <Component {...pageProps} />
       <FooterNote></FooterNote>
