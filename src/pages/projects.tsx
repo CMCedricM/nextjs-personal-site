8e1436ed15432659ddfb42745d8fedb32c1e01dc
Cedric Men <cedricm13@outlook.com>
2023-01-29 21:57:10 -0800

    added projects page, need to style

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,36 @@
 import { type NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
-const ProjectsPage = () => {
+const ProjectsPage : NextPage = () => {
+    
+    const [gitHubRepos, setGitHubRepos] = useState([]); 
+    const githubRepoUrl  = "https://api.github.com/users/CMCedricM/repos"
+
+    
+    const fetchRepos = () => {
+        fetch(githubRepoUrl).then((response) => response.json()).then((data) => {setGitHubRepos(data)})
+    }
+
+    useEffect(() => fetchRepos(), []); 
+
     return(
         <div className="min-w-full min-h-full grow flex flex-col items-center justify-center">
-                <text className="text-2xl text-white">This page is still under construction!</text>
+             <p className="text-2xl text-white">This page is still under construction!</p>
+            <div className="grid grid-row md:grid-cols-3 gap-4 justify-center items-center">
+            {gitHubRepos && 
+                gitHubRepos?.map((data : any, index) => {
+                    return (
+                    <div key={index}>
+                        <div className="flex flex-col items-center p-5 text-white">
+                            <h1>{data?.name}</h1>
+                            <p>{data?.description}</p>
+                            <p>{`Last Updated: ${data?.updated_at}`}</p>
+                        </div>
+                </div>)
+        })}
+            </div>
+               
         </div>
     )
 }
