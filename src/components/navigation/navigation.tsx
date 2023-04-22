704c3f645fc59720f091c055558a18b59166659a
Cedric Men <cedricm13@outlook.com>
2023-04-22 15:11:59 -0700

    updated resume

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,35 +1,36 @@
 import Link from "next/link";
 
 const Navigation = () => {
+  const pages: Record<string, string> = {
+    Home: "/",
+    Photography: "/photography",
+    Projects: "/projects",
+    Resume:
+      "https://drive.google.com/file/d/1vEoks0-8vLwm-XpZN-miRFSLZIw3nHG4/view?usp=sharing",
+  };
 
-
-    const pages : Record <string,string> = {
-        'Home' : '/', 
-        'Photography' : '/photography', 
-        'Projects' : '/projects',
-        'Resume' : 'https://drive.google.com/file/d/1OupL7Q8SEWKz_ZiMvbTG15rto4LwEkxM/view?usp=share_link'
-
-    }
-
-    return(
-        <div className="flex flex-col items-center text-white text-lg p-3">
-            <ul className="flex flex-row items-center gap-4">
-                |{
-                    Object.keys(pages).map((data, index) => {
-                        return(
-                                <ul key={index} className='flex flex-row items-center gap-4'>
-                                    <li>
-                                        <Link href={pages[data] || '' } className='hover:text-orange-400'>{data}</Link>
-                                    </li>|
-                                </ul>
-                       
-                        )
-                    })
-                }
+  return (
+    <div className="flex flex-col items-center p-3 text-lg text-white">
+      <ul className="flex flex-row items-center gap-4">
+        |
+        {Object.keys(pages).map((data, index) => {
+          return (
+            <ul key={index} className="flex flex-row items-center gap-4">
+              <li>
+                <Link
+                  href={pages[data] || ""}
+                  className="hover:text-orange-400"
+                >
+                  {data}
+                </Link>
+              </li>
+              |
             </ul>
-        </div>
-    )
-}
-
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default Navigation;
