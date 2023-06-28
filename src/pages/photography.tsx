c636d2740f02d8d31204c2e803d978f1f6a9933f
Cedric Men <cedricm13@outlook.com>
2023-06-27 22:38:06 -0700

    changed photo

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -1,8 +1,14 @@
 import { type NextPage } from "next";
-import { motion, AnimatePresence } from "framer-motion";
+import {
+  motion,
+  AnimatePresence,
+  domAnimation,
+  useAnimation,
+} from "framer-motion";
 import { variants, images } from "../styles/animations";
 import Image from "next/image";
 import image_alt from "../images/image-holder.png";
+import { useEffect, useState } from "react";
 
 type imageType = Record<string, string>;
 
@@ -34,36 +40,51 @@ const Photography: NextPage = () => {
       "https://filedn.com/lzwabVT0BiwB0zLQ0LYrokb/Photos/IMG_1507.JPG",
   };
 
+  const [showAnimation, setShowAnimation] = useState(false);
+  const [currentLoadedImgCnt, setCurrentImgLoadCnt] = useState<string[]>([]);
+
+  const handleLoad = async (img: string) => {
+    console.log(`Current`);
+    console.log(currentLoadedImgCnt);
+    const temp = [img, ...currentLoadedImgCnt];
+    console.log(temp);
+    setCurrentImgLoadCnt(temp);
+  };
+  useEffect(() => {
+    if (currentLoadedImgCnt.length >= Object.keys(imgURL).length - 1) {
+      setShowAnimation(true);
+      console.log("ready");
+    }
+  }, [currentLoadedImgCnt]);
+
   return (
     <div className="flex min-h-full min-w-full grow flex-col items-center justify-center">
-      <AnimatePresence>
-        {
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={variants}
-            className=" relative grid grow grid-rows-1 items-center justify-center gap-5 p-5 md:grid-cols-4"
-          >
-            {Object.keys(imgURL)?.map((data: string, index) => {
-              return (
-                <motion.li
-                  variants={images}
-                  key={index}
-                  className="flex grow flex-col"
-                >
-                  <Image
-                    src={imgURL[data] ?? image_alt}
-                    alt=""
-                    width={300}
-                    height={225}
-                    className="rounded-md transition duration-150  ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-2 hover:border-orange-400"
-                  ></Image>
-                </motion.li>
-              );
-            })}
-          </motion.div>
-        }
-      </AnimatePresence>
+      {
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className=" relative grid grow grid-rows-1 items-center justify-center gap-5 p-5 md:grid-cols-4"
+        >
+          {Object.keys(imgURL)?.map((data: string, index) => {
+            return (
+              <motion.li
+                variants={images}
+                key={index}
+                className="flex grow flex-col"
+              >
+                <Image
+                  src={imgURL[data] ?? image_alt}
+                  alt=""
+                  width={300}
+                  height={225}
+                  className={`rounded-md transition duration-150  ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-2 hover:border-orange-400`}
+                ></Image>
+              </motion.li>
+            );
+          })}
+        </motion.div>
+      }
     </div>
   );
 };
